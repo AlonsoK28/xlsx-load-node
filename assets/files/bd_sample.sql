8664e9e818258d75ee78f1b764573efe36829da4
C Alonso C Ortega <calonso011@yahoo.com.mx>
2018-10-03 12:38:27 -0500

    Added XLSX file / SQL script
    Added XLSX file / SQL script  for example of usage

---
+++ assets/files/bd_sample.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 03-10-2018 a las 19:37:11
+-- Versión del servidor: 10.1.29-MariaDB
+-- Versión de PHP: 7.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bd_sample`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `table_sample`
+--
+
+CREATE TABLE `table_sample` (
+  `NAME` varchar(500) NOT NULL,
+  `AGE` int(100) NOT NULL,
+  `PERSONAL_MAIL` varchar(500) NOT NULL,
+  `PHONE_1` int(100) NOT NULL,
+  `PHONE_2` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
